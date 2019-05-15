52ef1495e340b40f2b20596656faed004fa3fe22
Raphael Sales <raphael.melo21@gmail.com>
2019-05-15 00:28:01 -0300

    Ordering by create date

--- api/image/views.py
+++ api/image/views.py
@@ -9,5 +9,5 @@ from rest_framework import viewsets, views
 from rest_framework.parsers import MultiPartParser, FormParser
  
 class ImageViewSet(viewsets.ModelViewSet): 
-    queryset = Image.objects.all()
+    queryset = Image.objects.all().order_by('-created_at')
     serializer_class = ImageSerializer
